b7d945185f82713935732abd4750f70d6d00ffad
AfoDy <afodydigitals@gmail.com>
2023-12-01 18:44:52 -0800

    complete About component draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Hero from './Hero'
+import About from './About'
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <About />
     </div>
   )
 }
