2cf1a9afbdbf225cbd56d8186323c53fd1784848
AfoDy <afodydigitals@gmail.com>
2023-12-03 22:59:06 -0800

    complete Skills component

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -11,8 +11,8 @@ function Skills() {
           <img className='w-[32px] md:w-[43.71px] h-[43.62px]' src='./src/Graphics/CSS.svg' /> 
           <img className='w-[32px] md:w-[32px] h-[43.62px]' src='./src/Graphics/Wordpress.svg' /> 
           <img className='w-[32px] md:w-[43.71px] h-[43.62px]' src='./src/Graphics/React.svg' />
-          <img className='w-[32px] md:w-[43.71px] h-[43.62px]' src='./src/Graphics/Figma.svg' />
           <img className='w-[32px] md:w-[43.71px] h-[43.62px]' src='./src/Graphics/Tailwind.svg' />
+          <img className='w-[32px] md:w-[73.12px] h-[43.71px]' src='./src/Graphics/Figma.svg' />
           <img className='w-[32px] md:w-[43.71px] h-[43.62px]' src='./src/Graphics/AdobePhotoshop.svg' />
           <img className='w-[32px] md:w-[43.71px] h-[43.62px]' src='./src/Graphics/javascript.svg' />
           <img className='w-[32px] md:w-[43.71px] h-[43.62px]' src='./src/Graphics/AdobeXD.svg' />
