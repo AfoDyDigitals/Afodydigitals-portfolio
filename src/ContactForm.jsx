e3b3bf14d1e3884565ae7bfc057a05c1b8ab786c
AfoDy <afodydigitals@gmail.com>
2023-12-15 23:18:12 -0800

    complete contact form API integration

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -4,7 +4,7 @@ function ContactForm() {
   return (
     <div className="relative flex justify-center items-center">
       
-      <form action="#" method="POST" className="relative bg-black border-2 border-white flex flex-col justify-center items-center w-[340px] h-auto rounded-[20px] mx-[37px] mt-[95px] dark:text-white md:w-[1146px] h-auto mt-[157px] mx-[147px]">
+      <form action="https://formsubmit.co/3eb90342a33563ddc936921fdb53668a" method="POST" className="relative bg-black border-2 border-white flex flex-col justify-center items-center w-[340px] h-auto rounded-[20px] mx-[37px] mt-[95px] dark:text-white md:w-[1146px] h-auto mt-[157px] mx-[147px]">
       <img
         className="animate-pulse w-[106px] absolute top-[-50px] left-[40px] z-[1] md:top-[-50px] left-[-40px]"
         src="./src/Graphics/pattern.svg"
@@ -13,6 +13,9 @@ function ContactForm() {
         className="animate-pulse w-[106px] absolute top-[500px] right-[40px] z-[1] md:top-[485px] right-[-40px]"
         src="./src/Graphics/pattern.svg"
         />
+        <input type="hidden" name="_template" value="box"></input>
+        <input type="hidden" name="_subject" value="New Portfolio Webpage Enquiry!"></input>
+        <input type="hidden" name="_autoresponse" value="Thank you for stopping by, your email will be responded to asap!"></input>
         <div className="flex grid grid-cols-1 mx-[44px] mt-[11px] w-[251px] md:w-[789px] mt-[24px]  mx-[179px]">
           <div className="">
             <label
@@ -26,8 +29,10 @@ function ContactForm() {
                 type="text"
                 name="full-name"
                 id="full-name"
+                placeholder="Please input your full name"
                 autocomplete="full-name"
                 className="block bg-black border-b border-amber-500 w-full rounded-20 border-0 px-3.5 py-2 text-white shadow-sm  focus:ring-2 focus:ring-inset focus:ring-amber-500"
+                required
               />
             </div>
           </div>
@@ -44,8 +49,10 @@ function ContactForm() {
                 type="email"
                 name="email"
                 id="email"
+                placeholder="Please input your e-mail address"
                 autocomplete="email"
                 className="block bg-black border-b border-amber-500 w-full rounded-20 border-0 px-3.5 py-2 text-white shadow-sm  focus:ring-2 focus:ring-inset focus:ring-amber-500"
+                required
               />
             </div>
           </div>
@@ -61,8 +68,10 @@ function ContactForm() {
               <textarea
                 name="message"
                 id="message"
+                placeholder="Please enter your message"
                 rows="4"
                 className="block h-auto bg-black border-b border-amber-500 w-full rounded-20 border-0 px-3.5 py-2 text-white shadow-sm  focus:ring-2 focus:ring-inset focus:ring-amber-500 h-auto"
+                required
               ></textarea>
             </div>
           </div>
